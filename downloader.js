11c2e965d84acffcac2321218f9128f14ef04453
liamgen.js <liamgen.js@proton.me>
2024-01-05 12:29:34 +0100

    v0.1

--- downloader.js
+++ downloader.js
@@ -5,6 +5,28 @@
  Created:  2024-01-04T21:26:27.345Z 
 */
 
+/* Convertir base64 en jpg */
+function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+}
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 /* Aller chercher les images au lencement et au scroll */
 lookForImages()
 document.addEventListener("scroll", lookForImages);
@@ -40,10 +62,17 @@ function lookForImages(){
     let images = document.querySelectorAll("img");
     images.forEach((image) => {
         /* Si une image est visible, qu'elle n'a pas déjà été téléchargée et qu'elle à une source */
-        if(isVisible(image) && !image.getAttribute("downloaded-by-ext") && image.src){
+        if(isVisible(image) && !image.getAttribute("downloaded-by-ext") && image.src && image.className != "no69gc"){
             var filename = image.src.substring(image.src.lastIndexOf('/')+1);
+            let url = image.src
             /* On envoit le message au back de télécharger l'image */
-            chrome.runtime.sendMessage({url: image.src, filename: filename});
+            if(image.src.startsWith("data:image/"))
+            {
+                filename = makeid(5)+"-base64-decoded.png";
+                let file = dataURLtoFile(image.src)
+                url = URL.createObjectURL(file)
+            }
+            chrome.runtime.sendMessage({url: url, filename: filename});
             /* On définis l'image en tant que "déjà téléchargée" */
             image.setAttribute("downloaded-by-ext", "true");
         }
